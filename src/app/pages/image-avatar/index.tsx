be33b4f94e63804aa774afb0363822ec485e9279
Felipe Mendes <zo.fmendes@gmail.com>
2021-08-24 16:21:03 -0300

    :bug: fix image change propertie

--- src/app/pages/image-avatar/index.tsx
+++ src/app/pages/image-avatar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { ImageAvatar } from '../../../lib';
 
@@ -9,11 +9,28 @@ const Container = styled.div`
 	align-items: center;
 `;
 
+const IMAGE_URLS = [
+	'https://api.qa.smartepi.keymax.com.br/assets/product/aeris-77qvj-product:e9e7609d6c1c5fb52a35a48c9511b395.jpeg',
+	'https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg',
+];
+
 export const ImageAvatarPage = (): JSX.Element => {
+	const [avatar, setAvatar] = useState(0);
+
+	useEffect(() => {
+		setTimeout(() => {
+			if (avatar === 0) {
+				setAvatar(1);
+			} else {
+				setAvatar(0);
+			}
+		}, 5000);
+	}, [avatar]);
+
 	return (
 		<Container>
 			<h1>Image Avatar</h1>
-			<ImageAvatar src="/invalid" />
+			<ImageAvatar src={IMAGE_URLS[avatar]} size="300px" />
 		</Container>
 	);
 };
--- src/lib/components/ImageAvatar/index.tsx
+++ src/lib/components/ImageAvatar/index.tsx
@@ -13,18 +13,17 @@ export default function ImageAvatar(props: ImageAvatarProps): JSX.Element {
 	const [image, setImage] = useState(DefaultImage);
 
 	useEffect(() => {
+		setImage(DefaultImage);
 		if (props.src) {
 			const loadingImage = new Image();
 			const successCallback = (): void => {
 				setImage(props.src);
+				loadingImage.removeEventListener('load', successCallback);
 			};
 			loadingImage.addEventListener('load', successCallback);
 			loadingImage.src = props.src;
-			return () => {
-				loadingImage.removeEventListener('load', successCallback);
-			};
 		}
-	}, []);
+	}, [props.src]);
 
 	return <ImageAvatarElement {...props} src={image} />;
 }
