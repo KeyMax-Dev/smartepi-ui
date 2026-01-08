8a4999dc265e68f0025a5f9bbdc7229c29200075
Gustavo Baptista <gustavogsb@outlook.com>
2026-01-07 23:34:52 -0300

    chore: update dependencies and configuration for module support
    - Added "type": "module" to package.json for ES module support.
    - Updated peerDependencies to use newer versions of framer-motion, react, react-dom, and styled-components.
    - Updated devDependencies to include the latest versions of rollup plugins and TypeScript.
    - Refactored rollup.config.js to improve code readability and maintainability.
    - Changed import statements to use consistent double quotes.
    - Adjusted external dependencies in rollup.config.js for better clarity.

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,39 @@
-import typescript from '@rollup/plugin-typescript';
-import postcss from 'rollup-plugin-postcss';
-import image from '@rollup/plugin-image';
+import image from "@rollup/plugin-image";
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
 
-import pkg from './package.json';
+import pkg from "./package.json" with { type: "json" };
 
-const external = Object.keys(pkg.peerDependencies).concat(['path', 'fs', 'typescript']);
+const external = Object.keys(pkg.peerDependencies).concat([
+	"path",
+	"fs",
+	"typescript",
+]);
 
 console.log("external packages:", external);
 
 export default {
-  input: 'src/lib/index.ts',
-  plugins: [
-    typescript({ tsconfig: './src/lib/tsconfig.json', sourceMap: true }),
-    postcss({ plugins: [] }),
-    image()
-  ],
-  external,
-  output: [
-    {
-      format: 'cjs',
-      dir: './build',
-      sourcemap: true,
-      name: 'smartepi-ui',
-      exports: 'named',
-      esModule: false,
-      globals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        'styled-components': 'styled',
-      },
-      assetFileNames: 'src/lib/assets/export-files/**/*'
-    }
-  ]
+	input: "src/lib/index.ts",
+	plugins: [
+		typescript({ tsconfig: "./src/lib/tsconfig.json", sourceMap: true }),
+		postcss({ plugins: [] }),
+		image(),
+	],
+	external,
+	output: [
+		{
+			format: "cjs",
+			dir: "./build",
+			sourcemap: true,
+			name: "smartepi-ui",
+			exports: "named",
+			esModule: false,
+			globals: {
+				react: "React",
+				"react-dom": "ReactDOM",
+				"styled-components": "styled",
+			},
+			assetFileNames: "src/lib/assets/export-files/**/*",
+		},
+	],
 };
